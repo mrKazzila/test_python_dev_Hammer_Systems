04b3f90e0d0fd066566c1aed413c1fddfb611179
mrKazzila <mrkazzila@gmail.com>
2023-08-11 15:52:17 +0300

    feat: add users & referral services

--- app/referrals/referral_services.py
+++ app/referrals/referral_services.py
@@ -5,4 +5,13 @@ from string import ascii_lowercase, ascii_uppercase, digits
 def generate_referral_code(length: int = 6) -> str:
     """Generate a random referral code of the specified length."""
     chars = ascii_uppercase + ascii_lowercase + digits
+
     return ''.join(choice(chars) for _ in range(length))
+
+
+def create_referral_code_for_user(user_obj, referral_code_model):
+    """Create referral code for user."""
+    referral, _ = referral_code_model.objects.get_or_create(owner=user_obj)
+    code = str(referral.referral_code)
+
+    return code
--- app/users/user_services.py
+++ app/users/user_services.py
@@ -1,7 +1,33 @@
+import logging
 import random
 from string import digits
+
+from django.contrib.auth.tokens import default_token_generator
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND
+
+logger = logging.getLogger(__name__)
 
 
 def generate_activation_code():
     """Generate a random activation code."""
     return ''.join(random.choice(digits) for _ in range(4))
+
+
+def generate_token_for_user(user_obj, user_model):
+    """Generate token for user."""
+    user = user_model.objects.get(username=user_obj)
+    token = default_token_generator.make_token(user)
+
+    return token
+
+
+def delete_referral_code_for_authenticated_user(referral_obj):
+    """Delete the referral code for the authenticated user."""
+    if referral_obj:
+        referral_obj.delete()
+        logger.info('Referral code deleted')
+        return Response('Referral code successfully deleted', status=HTTP_204_NO_CONTENT)
+
+    logger.warning('Referral code not found')
+    return Response('Referral code not found', status=HTTP_404_NOT_FOUND)
